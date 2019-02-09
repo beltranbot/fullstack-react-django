cac5a9db3c59b7269fec62170f1496b4ae85a024
Carlos Beltran <beltranbot@gmail.com>
2019-02-09 16:12:25 -0500

    implementing react

---
+++ leadmanager/frontend/src/components/Leads/Form.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Form extends Component {
+  render () {
+    return (
+      <div className=''>
+        <h1>Add lead form</h1>
+      </div>
+    )
+  }
+}
+
+export default Form
