47503b5f26590a13bab5a392e9f3aa52f5025a7d
Carlos Beltran <beltranbot@gmail.com>
2019-02-10 07:53:57 -0500

    Redux & HTTP - list leads and delete leads

--- leadmanager/frontend/src/components/Leads/Leads.js
+++ leadmanager/frontend/src/components/Leads/Leads.js
@@ -1,13 +1,63 @@
-import React, {Component} from 'react'
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+
+import PropTypes from 'prop-types'
+import { getLeads, deleteLead } from '../../actions/leads'
 
 class Leads extends Component {
-  render () {
+  static propTypes = {
+    leads: PropTypes.array.isRequired
+  }
+
+  componentDidMount() {
+    this.props.getLeads()
+  }
+
+
+  render() {
+    let leads = []
+    if (this.props.leads) {
+      leads = this.props.leads.map(lead => {
+        return (
+          <tr key={lead.id}>
+            <td>{lead.id}</td>
+            <td>{lead.name}</td>
+            <td>{lead.email}</td>
+            <td>{lead.message}</td>
+            <td>
+              <button
+                className="btn btn-danger btn-sm"
+                onClick={this.props.deleteLead.bind(this, lead.id)}
+              >Delete</button>
+            </td>
+          </tr>
+        )
+      })
+    }
     return (
-      <div className=''>
-        <h1>Leads List</h1>
-      </div>
+      <Fragment>
+        <h2>Leads</h2>
+        <table className="table table-stripped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {leads}
+          </tbody>
+        </table>
+      </Fragment>
     )
   }
 }
 
-export default Leads
+const mapStateToProps = state => ({
+  leads: state.leads.leads
+})
+
+export default connect(mapStateToProps, { getLeads, deleteLead })(Leads)
