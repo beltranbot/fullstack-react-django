75c3107111e3fbd58ef3044f0ddcc4602bc31f8f
Carlos Beltran <beltranbot@gmail.com>
2019-02-10 08:27:49 -0500

    Redux & HTTP - Adding leads form

--- leadmanager/frontend/src/reducers/leads.js
+++ leadmanager/frontend/src/reducers/leads.js
@@ -1,4 +1,4 @@
-import { GET_LEADS, DELETE_LEAD } from '../actions/types.js'
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from '../actions/types.js'
 
 const initialState = {
   leads: []
@@ -15,6 +15,13 @@ export default function (state = initialState, action) {
       return {
         ...state,
         leads: state.leads.filter(lead => lead.id !== action.payload)
+      }
+    case ADD_LEAD:
+      console.log('now test here')
+      let leads = [...state.leads, action.payload]
+      return {
+        ...state,
+        leads
       }
   
     default:
